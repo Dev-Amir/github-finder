7b1300ac456fd822809658c25f8b41e4bc15a293
Amirreza Hemmati <dev.amirhemmati1382@gmail.com>
2020-03-11 17:17:50 -0700

    Updates

---
+++ precache-manifest.63a86e71cb7c30d80568fb13d38aa141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa2b50b185a92d30f3a6d9acedab13",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "5de92ec53cb925ed04f5",
+    "url": "/github-finder/static/css/main.69a69a0f.chunk.css"
+  },
+  {
+    "revision": "202139508546ea89b7fd",
+    "url": "/github-finder/static/js/2.d581e3c6.chunk.js"
+  },
+  {
+    "revision": "5de92ec53cb925ed04f5",
+    "url": "/github-finder/static/js/main.86d311d3.chunk.js"
+  },
+  {
+    "revision": "779661bbfa34baa3def5",
+    "url": "/github-finder/static/js/runtime-main.cfb2fad8.js"
+  }
+]);
